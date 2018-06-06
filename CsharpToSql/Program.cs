258dfb7e928b88749982afd9dcfd068b97050986
Foster Stule <camodude5@yahoo.com>
2018-06-06 11:32:52 -0400

    made a connection to the sql databass prs and opened the connection succefully

--- CsharpToSql/Program.cs
+++ CsharpToSql/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,13 @@ namespace CsharpToSql
 	{
 		static void Main(string[] args)
 		{
+			string connStr = @"server=DESKTOP-7KOO68T\SQLEXPRESS;database=prs;Trusted_connection=true";
+			SqlConnection conn = new SqlConnection(connStr);
+			conn.Open();
+			if (conn.State != System.Data.ConnectionState.Open) {
+				throw new ApplicationException("connection did not open");
+			}
+			System.Diagnostics.Debug.WriteLine("Connection Opened");
 		}
 	}
 }
