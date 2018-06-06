e04ffce0a76a56b6680702ed21d85361ee10c651
Foster Stule <camodude5@yahoo.com>
2018-06-06 14:03:11 -0400

    made it so it can retrieve data fromt the data base

--- CsharpToSql/Program.cs
+++ CsharpToSql/Program.cs
@@ -9,6 +9,7 @@ namespace CsharpToSql
 {
 	class Program
 	{
+		static List<User> users = new List<User>();
 		static void Main(string[] args)
 		{
 			string connStr = @"server=DESKTOP-7KOO68T\SQLEXPRESS;database=prs;Trusted_connection=true";
@@ -21,9 +22,29 @@ namespace CsharpToSql
 			SqlCommand cmd = new SqlCommand(sql, conn);
 			SqlDataReader reader = cmd.ExecuteReader();
 			while (reader.Read()) {
-				int id = reader.GetInt32(reader.GetOrdinal("Id"));
+				int? id = reader.GetInt32(reader.GetOrdinal("Id"));
 				string username = reader.GetString(reader.GetOrdinal("Username"));
-				System.Diagnostics.Debug.WriteLine($"{id},{username}");
+				string password = reader.GetString(reader.GetOrdinal("Password"));
+				string first = reader.GetString(reader.GetOrdinal("Firstname"));
+				string last = reader.GetString(reader.GetOrdinal("Lastname"));
+				string phone = reader.GetString(reader.GetOrdinal("phone"));
+				string email = reader.GetString(reader.GetOrdinal("email"));
+				bool reviewer = reader.GetBoolean(reader.GetOrdinal("IsReviewer"));
+				bool admin = reader.GetBoolean(reader.GetOrdinal("IsAdmin"));
+				bool active = reader.GetBoolean(reader.GetOrdinal("Active"));
+				User user = new User();
+				user.Id = id;
+				user.Username = username;
+				user.Password = password;
+				user.Firstname = first;
+				user.Lastname = last;
+				user.Phone = phone;
+				user.Email = email;
+				user.IsReviewer = reviewer;
+				user.IsAdmin = admin;
+				user.Active = active;
+				users.Add(user);
+				System.Diagnostics.Debug.WriteLine($"{id},{username},{password},{first},{last},{phone},{email},{reviewer},{admin},{active}");
 			}
 
 			conn.Close();
